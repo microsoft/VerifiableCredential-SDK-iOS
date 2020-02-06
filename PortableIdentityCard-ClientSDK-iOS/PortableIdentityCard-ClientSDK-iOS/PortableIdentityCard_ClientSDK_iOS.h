c6884741572ddfbc7f7155dc2d00c7bb54fcc5a7
sydneymorton <symorton@microsoft.com>
2020-02-06 08:50:39 -0800

    init for iOS framework

---
+++ PortableIdentityCard-ClientSDK-iOS/PortableIdentityCard-ClientSDK-iOS/PortableIdentityCard_ClientSDK_iOS.h
@@ -0,0 +1,19 @@
+//
+//  PortableIdentityCard_ClientSDK_iOS.h
+//  PortableIdentityCard-ClientSDK-iOS
+//
+//  Created by Sydney Morton on 2/6/20.
+//  Copyright © 2020 Microsoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PortableIdentityCard_ClientSDK_iOS.
+FOUNDATION_EXPORT double PortableIdentityCard_ClientSDK_iOSVersionNumber;
+
+//! Project version string for PortableIdentityCard_ClientSDK_iOS.
+FOUNDATION_EXPORT const unsigned char PortableIdentityCard_ClientSDK_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PortableIdentityCard_ClientSDK_iOS/PublicHeader.h>
+
+
---
+++ PortableIdentityCard-ClientSDK-iOS/PortableIdentityCard-ClientSDK-iOSTests/PortableIdentityCard_ClientSDK_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  PortableIdentityCard_ClientSDK_iOSTests.swift
+//  PortableIdentityCard-ClientSDK-iOSTests
+//
+//  Created by Sydney Morton on 2/6/20.
+//  Copyright © 2020 Microsoft. All rights reserved.
+//
+
+import XCTest
+@testable import PortableIdentityCard_ClientSDK_iOS
+
+class PortableIdentityCard_ClientSDK_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
