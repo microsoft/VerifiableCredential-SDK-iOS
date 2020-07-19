1f15e74135d1c3a667766fea170c76f10dd860e3
sydneymorton <symorton@microsoft.com>
2020-07-19 14:50:26 -0400

    Add sdk workspace for separate projects.

---
+++ networking/Pods/Target Support Files/Pods-networking-networkingTests/Pods-networking-networkingTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_networking_networkingTests : NSObject
+@end
+@implementation PodsDummy_Pods_networking_networkingTests
+@end
---
+++ networking/networking/networking.h
@@ -0,0 +1,19 @@
+//
+//  networking.h
+//  networking
+//
+//  Created by Sydney Morton on 7/19/20.
+//  Copyright © 2020 Microsoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for networking.
+FOUNDATION_EXPORT double networkingVersionNumber;
+
+//! Project version string for networking.
+FOUNDATION_EXPORT const unsigned char networkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <networking/PublicHeader.h>
+
+
