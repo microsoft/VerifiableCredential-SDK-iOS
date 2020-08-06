547d89f15b268a6e0e78194b4b3cee93170574ad
Daniel Godbout <dangodb@WU2TEHPV08CX-07.redmond.corp.microsoft.com>
2020-08-06 11:16:26 -0700

    Add VCCrypto project to sdk
    Add project and pod dependencies to the workspace.

---
+++ VCCrypto/VCCrypto/VCCrypto.h
@@ -0,0 +1,19 @@
+//
+//  VCCrypto.h
+//  VCCrypto
+//
+//  Created by Daniel Godbout on 8/6/20.
+//  Copyright © 2020 Daniel Godbout. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VCCrypto.
+FOUNDATION_EXPORT double VCCryptoVersionNumber;
+
+//! Project version string for VCCrypto.
+FOUNDATION_EXPORT const unsigned char VCCryptoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VCCrypto/PublicHeader.h>
+
+
