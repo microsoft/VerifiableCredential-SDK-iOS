81a959bf0ee29f1b20d5b54840e8a76674cbee7b
sydneymorton <symorton@microsoft.com>
2020-09-14 15:42:10 -0700

    Add UseCAse

--- VCUseCase/VCUseCase/IssuanceUseCase.swift
+++ VCUseCase/VCUseCase/IssuanceUseCase.swift
@@ -11,22 +11,21 @@ import VcNetworking
 import VcJwt
 
 class IssuanceUseCase {
-    
+
     let repo = IssuanceRepository()
     let signer = Secp256k1Signer()
-    let verifier = Secp256k1Verifier()
-    
+    //let verifier = Secp256k1Verifier()
+
     func getRequest(usingUrl url: String) -> Promise<Contract> {
         return self.repo.getRequest(withUrl: url)
     }
-    
-    func sendResponse(token: inout JwsToken<IssuanceResponseClaims>, usingUrl url: String) throws -> Promise<VerifiableCredential> {
-        // create Issuance Response Claims with everything needed and wrap in JWS token
-        // sign that token with key related to DID that wants to use
-        // send response and get VerifiableCredential back... return with Contract?
-        let keyId = KeyId(id: UUID())
-        try token.sign(using: signer, withSecret: keyId)
-        return self.repo.sendResponse(usingUrl: url, withBody: token)
-    }
-    
+
+//    func sendResponse(token: inout JwsToken<IssuanceResponseClaims>, usingUrl url: String) throws -> Promise<VerifiableCredential> {
+//        // create Issuance Response Claims with everything needed and wrap in JWS token
+//        // sign that token with key related to DID that wants to use
+//        // send response and get VerifiableCredential back... return with Contract?
+//        let keyId = KeyId(id: UUID())
+//        try token.sign(using: signer, withSecret: keyId)
+//        return self.repo.sendResponse(usingUrl: url, withBody: token)
+//    }
 }
