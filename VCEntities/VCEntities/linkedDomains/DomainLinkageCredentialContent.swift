2a46f3e1b25bbd64cb8ed759935d231629341dfc
sydneymorton <symorton@microsoft.com>
2021-02-01 14:58:23 -0800

    Clean up files

--- VCEntities/VCEntities/linkedDomains/DomainLinkageCredentialContent.swift
+++ VCEntities/VCEntities/linkedDomains/DomainLinkageCredentialContent.swift
@@ -3,12 +3,16 @@
 *  Licensed under the MIT License. See License.txt in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 
-public struct DomainLinkageCredentialSubject: Codable {
-    let did: String
-    let domainUrl: String
+public struct DomainLinkageCredentialContent: Codable {
+    let context: [String]
+    let issuer: String
+    let issuanceDate: String
+    let expirationDate: String
+    let type: [String]
+    let credentialSubject: DomainLinkageCredentialSubject
     
     enum CodingKeys: String, CodingKey {
-        case did = "id"
-        case domainUrl = "origin"
+        case context = "@context"
+        case issuer, issuanceDate, expirationDate, type, credentialSubject
     }
 }
