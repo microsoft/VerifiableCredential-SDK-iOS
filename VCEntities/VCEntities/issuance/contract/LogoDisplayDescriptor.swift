8a6226bf9cc90a56e8709e202f0dc10dd82bf7b9
Daniel Godbout <dangodb@microsoft.com>
2021-02-19 14:14:54 -0800

    add optional image property to LogoDisplayDescriptor

--- VCEntities/VCEntities/issuance/contract/LogoDisplayDescriptor.swift
+++ VCEntities/VCEntities/issuance/contract/LogoDisplayDescriptor.swift
@@ -8,8 +8,12 @@ public struct LogoDisplayDescriptor: Codable, Equatable {
     public let uri: String?
     public let logoDescription: String?
 
+    // Image can be already included in the logo display descriptor as a base64 encoded png
+    public let image: String?
+
     enum CodingKeys: String, CodingKey {
         case uri
         case logoDescription = "description"
+        case image
     }
 }
