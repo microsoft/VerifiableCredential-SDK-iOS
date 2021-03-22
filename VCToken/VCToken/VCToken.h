a69f78748ca415a026049dc576047bf86ab6cebb
sydneymorton <symorton@microsoft.com>
2021-03-22 12:37:58 -0500

    Update header file

--- VCToken/VCToken/VCToken.h
+++ VCToken/VCToken/VCToken.h
@@ -1,10 +1,7 @@
-//
-//  VCToken.h
-//  VCToken
-//
-//  Created by Sydney Morton on 8/25/20.
-//  Copyright © 2020 Microsoft. All rights reserved.
-//
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
 
 #import <Foundation/Foundation.h>
 
