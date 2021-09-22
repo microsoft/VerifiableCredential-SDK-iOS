88951a363539cc0c137cd5c4f9b0ff430c2125b4
sydneymorton <symorton@microsoft.com>
2021-09-22 10:49:30 -0400

    Update String+PercentEncoding.swift

--- VCNetworking/VCNetworking/extensions/String+PercentEncoding.swift
+++ VCNetworking/VCNetworking/extensions/String+PercentEncoding.swift
@@ -7,7 +7,7 @@ import Foundation
 
 extension String {
   func stringByAddingPercentEncodingForRFC3986() -> String? {
-    let unreserved = ":-._~/?%"
+    let unreserved = ":-._~/?%="
     let allowed = NSMutableCharacterSet.alphanumeric()
     allowed.addCharacters(in: unreserved)
     return addingPercentEncoding(withAllowedCharacters: allowed as CharacterSet)
