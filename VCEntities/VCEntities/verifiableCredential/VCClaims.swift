d67470d1880df065bbe494d912cc50c95a416a02
Sydney Morton <symorton@microsoft.com>
2022-01-05 15:59:18 -0800

    Update VC data models to be optional

--- VCEntities/VCEntities/verifiableCredential/VCClaims.swift
+++ VCEntities/VCEntities/verifiableCredential/VCClaims.swift
@@ -5,13 +5,19 @@
 
 import VCToken
 
+/**
+ * Contents of a Verifiable Credential Token.
+ *
+ * @see [Verifiable Credential Spec](https://www.w3.org/TR/vc-data-model/)
+ */
 public struct VCClaims: Claims {
-    public let jti: String
-    public let iss: String
-    public let sub: String
-    public let iat: Double
-    public let exp: Double
-    public let vc: VerifiableCredentialDescriptor
+    public let jti: String?
+    public let iss: String?
+    public let sub: String?
+    public let iat: Double?
+    public let exp: Double?
+    public let vc: VerifiableCredentialDescriptor?
 }
 
+/// JWSToken representation of a Verifiable Credential.
 public typealias VerifiableCredential = JwsToken<VCClaims>
