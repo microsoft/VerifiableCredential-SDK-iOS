03b7f164176cc0e66a2531559b9967c44a6be962
Sydney Morton <symorton@microsoft.com>
2022-04-20 10:14:49 -0700

    Fix tests

--- VCServices/VCServicesTests/IdentifierDatabaseTests.swift
+++ VCServices/VCServicesTests/IdentifierDatabaseTests.swift
@@ -16,7 +16,7 @@ class IdentifierDatabaseTests: XCTestCase {
     var identifierDB: IdentifierDatabase!
     
     override func setUpWithError() throws {
-        let cryptoOperations = CryptoOperations(sdkConfiguration: VCSDKConfiguration.sharedInstance)
+        let cryptoOperations = CryptoOperations(sdkConfiguration: MockVCSDKConfiguration())
         identifierDB = IdentifierDatabase(cryptoOperations: cryptoOperations)
         try CoreDataManager.sharedInstance.deleteAllIdentifiers()
         identifierCreator = IdentifierCreator(cryptoOperations: cryptoOperations)
