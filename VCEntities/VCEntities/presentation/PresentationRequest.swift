ca2c2db2f002301d21845b112dfc53545a8996c0
Sydney Morton <symorton@microsoft.com>
2023-02-14 11:10:57 -0800

    Move pin to request.

--- VCEntities/VCEntities/presentation/PresentationRequest.swift
+++ VCEntities/VCEntities/presentation/PresentationRequest.swift
@@ -20,7 +20,7 @@ public struct PresentationRequest {
     }
     
     public func getPinRequiredLength() -> Int? {
-        return token.content.idTokenHint?.token.content.pin?.length
+        return token.content.pin?.length
     }
     
     public func containsRequiredClaims() -> Bool {
@@ -28,6 +28,6 @@ public struct PresentationRequest {
     }
     
     public func getPinSalt() -> String? {
-        return token.content.idTokenHint?.token.content.pin?.salt
+        return token.content.pin?.salt
     }
 }
